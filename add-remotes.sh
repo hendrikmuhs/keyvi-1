c2eb82eccd8b23620c6e46bf1aaa300a2d5661b2
Hendrik Muhs <hendrik.muhs@gmail.com>
2022-04-30 21:50:11 +0200

    prepare adding morton-nd as 3rdparty library

--- add-remotes.sh
+++ add-remotes.sh
@@ -6,3 +6,4 @@ git remote add rapidjson    https://github.com/Tencent/rapidjson.git
 git remote add msgpack-c    https://github.com/msgpack/msgpack-c.git
 git remote add tiny-process-library https://gitlab.com/eidheim/tiny-process-library
 git remote add utfcpp       https://github.com/nemtrif/utfcpp.git
+git remote add morton-nd    https://github.com/morton-nd/morton-nd.git
---
+++ keyvi/3rdparty/pull-morton-nd.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+git subtree pull --prefix keyvi/3rdparty/morton-nd morton-nd main --squash
